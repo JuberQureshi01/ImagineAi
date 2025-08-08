5046730b95d61972ab4d59c87379737bde77fae6
Juber Qureshi <142721071+JuberQureshi01@users.noreply.github.com>
2025-08-09 02:06:43 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
  const api = axios.create({
-  baseURL: 'https://imagineai-il4p.onrender.com',
+  baseURL: 'https://imagineaii.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
